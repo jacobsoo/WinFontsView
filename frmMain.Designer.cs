b7250c2a8b3bed35bd529bfc3a359d50cd362dfe
Jacob Soo <jacobsoo@users.noreply.github.com>
2015-05-11 22:32:09 +0800

    Updates

---
+++ frmMain.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WinFontsView
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.ss = new System.Windows.Forms.StatusStrip();
+            this.tslFonts = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dgvFonts = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ss.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFonts)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ss
+            // 
+            this.ss.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslFonts});
+            this.ss.Location = new System.Drawing.Point(0, 380);
+            this.ss.Name = "ss";
+            this.ss.Size = new System.Drawing.Size(569, 22);
+            this.ss.TabIndex = 1;
+            this.ss.Text = "statusStrip1";
+            // 
+            // tslFonts
+            // 
+            this.tslFonts.Name = "tslFonts";
+            this.tslFonts.Size = new System.Drawing.Size(0, 17);
+            // 
+            // dgvFonts
+            // 
+            this.dgvFonts.AllowUserToDeleteRows = false;
+            this.dgvFonts.AllowUserToOrderColumns = true;
+            this.dgvFonts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFonts.Location = new System.Drawing.Point(12, 27);
+            this.dgvFonts.Name = "dgvFonts";
+            this.dgvFonts.ReadOnly = true;
+            this.dgvFonts.Size = new System.Drawing.Size(545, 339);
+            this.dgvFonts.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(569, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 402);
+            this.Controls.Add(this.dgvFonts);
+            this.Controls.Add(this.ss);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WinFontsView";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ss.ResumeLayout(false);
+            this.ss.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFonts)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip ss;
+        private System.Windows.Forms.ToolStripStatusLabel tslFonts;
+        private System.Windows.Forms.DataGridView dgvFonts;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Text;
+
+namespace WinFontsView
+{
+    public partial class frmMain : Form
+    {
+        public frmMain(){
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e){
+            int iFonts = 0;
+            DataTable table = new DataTable();
+            dgvFonts.DataSource = table;
+            table.Columns.Add("Fonts", typeof(string));
+            table.Columns.Add("Sample", typeof(string));
+
+            InstalledFontCollection fonts = new InstalledFontCollection();
+            iFonts = fonts.Families.Length;
+            while (table.Rows.Count < iFonts){
+                table.Rows.Add();
+            }
+            List<string> list = new List<string>();
+            try{
+                foreach (FontFamily font in fonts.Families){
+                    list.Add(font.Name);
+                }
+            }catch{
+            }
+
+            for (int a = 0; a < iFonts; a++){
+                table.Rows[a][0] = list[a];
+                FontFamily currentFont = new FontFamily(list[a].ToString());
+                if (currentFont.IsStyleAvailable(FontStyle.Regular)){
+                    dgvFonts.Rows[a].Cells[1].Style.Font = new Font(list[a].ToString(), 14, FontStyle.Regular, GraphicsUnit.Point);
+                }else if (currentFont.IsStyleAvailable(FontStyle.Bold)){
+                    dgvFonts.Rows[a].Cells[1].Style.Font = new Font(list[a].ToString(), 14, FontStyle.Bold, GraphicsUnit.Point);
+                }else if (currentFont.IsStyleAvailable(FontStyle.Italic)){
+                    dgvFonts.Rows[a].Cells[1].Style.Font = new Font(list[a].ToString(), 14, FontStyle.Italic, GraphicsUnit.Point);
+                }
+                table.Rows[a][1] = "Hello World! The font size is 14.";
+            }
+            dgvFonts.AutoResizeColumns();
+            tslFonts.Text = iFonts + " fonts installed";
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e){
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("WinFontsView by Jacob Soo", "About");
+        }
+    }
+}
